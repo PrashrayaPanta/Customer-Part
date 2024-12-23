6cb27363b4a59ed16363e7b122cc674e259eba60
PrashrayaPanta <prashrayapanta33@gmail.com>
2024-12-23 16:41:52 +0545

    forbigscreencontentisstacked

--- src/components/CarouselComponent.jsx
+++ src/components/CarouselComponent.jsx
@@ -1,65 +1,65 @@
-import React from 'react'
-import { Carousel, CarouselItem } from 'react-bootstrap'
+import React from "react";
+import { Carousel, CarouselItem } from "react-bootstrap";
 
 const CarouselComponent = () => {
-
-
-    const ImageURLArray = ["./slider-1.jpg" , "./slider-2.jpg", "./slider-3.jpg"];
-
+  const ImageURLArray = ["./slider-1.jpg", "./slider-2.jpg", "./slider-3.jpg"];
 
   return (
-        <>
-        
-        <div id="slider" class="carousel slide w-100" data-bs-ride="carousel">
-            <ol class="carousel-indicators">
-              <li
-                data-bs-target="#slider"
-                data-bs-slide-to="0"
-                class="active"
-              ></li>
-              <li data-bs-target="#slider" data-bs-slide-to="1"></li>
-              <li data-bs-target="#slider" data-bs-slide-to="2"></li>
-            </ol>
-            <div class="carousel-inner" role="listbox">
-              <Carousel>
-                {
-                   ImageURLArray.map((eachimgurl) =>      
-                    <CarouselItem>
-                             <img src={eachimgurl} class="slider-img" />
-                    </CarouselItem>
-                
-                    )
-                }
-              </Carousel>
-            </div>
-            <button
-              class="carousel-control-prev"
-              type="button"
-              data-bs-target="#slider"
-              data-bs-slide="prev"
-            >
-              <span
-                class="carousel-control-prev-icon"
-                aria-hidden="true"
-              ></span>
-              <span class="visually-hidden">Previous</span>
-            </button>
-            <button
-              class="carousel-control-next"
-              type="button"
-              data-bs-target="#slider"
-              data-bs-slide="next"
-            >
-              <span
-                class="carousel-control-next-icon"
-                aria-hidden="true"
-              ></span>
-              <span class="visually-hidden">Next</span>
-            </button>
+    <>
+      <div id="slider" class="carousel slide w-100" data-bs-ride="carousel">
+        <ol class="carousel-indicators">
+          <li
+            data-bs-target="#slider"
+            data-bs-slide-to="0"
+            class="active bg-primary"
+          >
+            Hello
+          </li>
+          <li data-bs-target="#slider" data-bs-slide-to="1"></li>
+          <li data-bs-target="#slider" data-bs-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner" role="listbox">
+          <Carousel>
+            {ImageURLArray.map((eachimgurl) => (
+              <CarouselItem>
+                <div class="image-stacked-content">
+                  <div>
+                    <img
+                      src={eachimgurl}
+                      class="slider-img"
+              
+                    />
+                  </div>
+                  <div class="stacked-content">
+                      <h1>We are good </h1>
+                      <h1>IKJDSAKFLSAKDJGFLAKDGKL</h1>
+                  </div>
+                </div>
+              </CarouselItem>
+            ))}
+          </Carousel>
         </div>
-        
-        </>
-  )
-}
+        <button
+          class="carousel-control-prev"
+          type="button"
+          data-bs-target="#slider"
+          data-bs-slide="prev"
+        >
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button
+          class="carousel-control-next"
+          type="button"
+          data-bs-target="#slider"
+          data-bs-slide="next"
+        >
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </div>
+    </>
+  );
+};
 
-export default CarouselComponent
+export default CarouselComponent;
